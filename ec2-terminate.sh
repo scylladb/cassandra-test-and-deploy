04a1768ccc5b3c04f949a343431db2d68e7b5f51
Tzach Livyatan <tzach@cloudius-systems.com>
2015-10-20 13:12:59 +0300

    clean up terminate script

--- ec2-terminate.sh
+++ ec2-terminate.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ansible-playbook -i inventories/ec2/ terminate.yaml --limit "tag_user_`echo $ANSIBLE_EC2_PREFIX`"
+ansible-playbook -i inventories/ec2/ terminate.yaml --limit "tag_user_`echo $ANSIBLE_EC2_PREFIX`" "$@"
