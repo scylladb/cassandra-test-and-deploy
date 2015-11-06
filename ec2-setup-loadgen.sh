1db6878cc1fdc41d2551e740eb5715ad89991ced
Tzach Livyatan <tzach@cloudius-systems.com>
2015-11-06 15:26:42 +0200

    Graphite server instance is no longer in use

--- ec2-setup-loadgen.sh
+++ ec2-setup-loadgen.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ansible-playbook -i inventories/ec2/ setup-ec2-loadgen.yaml -e "collect_server_ip=`./ec2-graphite-ip`" -e "login_user=fedora" "$@"
+ansible-playbook -i inventories/ec2/ setup-ec2-loadgen.yaml -e "login_user=fedora" "$@"
