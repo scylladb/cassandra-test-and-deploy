e6056d00aa999a03bbc27d133152233d404346e1
Tzach Livyatan <tzach@cloudius-systems.com>
2015-12-20 14:03:47 +0200

    refactor add node

--- ec2-add-node-to-cluster.sh
+++ ec2-add-node-to-cluster.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ansible-playbook -i inventories/ec2/ setup-ec2-cassandra.yaml -e "add_node=true" -e "use_reflector=false" "$@"
+ansible-playbook -i inventories/ec2/ ec2-add-node.yaml "$@"
