e90f6da257a804eebec737d80eb2afed51f506f1
Tzach Livyatan <tzach@scylladb.com>
2016-02-08 16:43:32 +0200

    add user prefix to setup scripts

--- ec2-setup-cassandra.sh
+++ ec2-setup-cassandra.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ansible-playbook setup-ec2-cassandra.yaml -e "@inventories/ec2/group_vars/all.yaml" "$@"
+ansible-playbook setup-ec2-cassandra.yaml -e "@inventories/ec2/group_vars/all.yaml" -e "setup_name=`echo $ANSIBLE_EC2_PREFIX`" "$@"
